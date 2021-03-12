028a714e9e8fbec7e2e055aa08ca64a6af4ead88
Luca <memeoluca6@gmail.com>
2021-03-12 15:43:27 +0100

    implemented bullet change

--- main.js
+++ main.js
@@ -2,11 +2,17 @@
 var nextImageBtn = $(".slider-wrapper .fas.fa-angle-right");
 
 nextImageBtn.click(function() {
+  // get active img from the document
   activeImg = $(".images .active");
   activeImg.removeClass("active");
+  // get active bullet from the document
+  activeBullet = $(".nav .fa-circle.active");
+  activeBullet.removeClass("active");
   if(activeImg.hasClass("last") == true) {
     $(".images .first").addClass("active");
+    $(".nav .first").addClass("active");
   } else {
     activeImg.next().addClass("active");
+    activeBullet.next().addClass("active");
   }
 });
